8ff1819c261fe3757e2f57e6ed1c0a06f4f9c483
Kirill Kobzev <kirill.kobzev5555@gmail.com>
2021-12-18 00:46:10 +0300

    update main.py and TZ in README.md

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
 import pygame
 from intro import game_intro
 #from end import end_game
+'''from levels import Level_1, Level_2, Level_3
+from player import Player'''
 
-pygame.init()
-width, height = 800, 600
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption('Gold exaction')
 
-def main():
-    running = True
+def main():  # основная функция игры
+    pygame.init()  #  Инициализация
+    width, height = 800, 600  # размеры окна
+    screen = pygame.display.set_mode((width, height))  # установка размеров окна
+    pygame.display.set_caption('Gold exaction')  # заголовок окна
+    running = True  # основной цикл игры
+    '''player = Player()  # создание героя
+    levels = []  # создание уровней
+    levels.append(Level_1(player))  # добавление героя'''
+
     while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+        for event in pygame.event.get():  # отслеживание действий
+            if event.type == pygame.QUIT:  # при закрытии прекращается цикл
                 running = False
-                pygame.quit()
         if running:
             game_intro()
+    pygame.quit()  # закртытие программы
 
 
-
-if __name__ == '__main__':
+if __name__ == '__main__':  # запуск игры
     main()
